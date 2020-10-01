50cf192504488341ea0e07d9e0c8a9fa3ce5e5c8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-01 21:01:12 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/learning/springkafkadocker/SpringKafkaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.springkafkadocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/learning/springkafkadocker/SpringKafkaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.springkafkadocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
